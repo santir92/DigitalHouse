39643e05f318d93722c50f679bcadfe1b038fc85
JerezC <jajerezc.jj@gmail.com>
2022-09-04 21:46:19 -0500

    archivos

---
+++ app.js
@@ -0,0 +1,24 @@
+// Importación módulos
+
+const express = require('express')
+const path = require('path')
+
+// Definición app
+
+const app = express()
+
+// Indicar que la carpeta public es de recursos estáticos
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+// Poner a correr el servidor en el puerto 3001
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log('3001')
+})
+
+// Vista home
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/restaurante.html'))
+})
