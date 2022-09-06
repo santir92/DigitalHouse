767ddb02a926929de177eb85f46224b512d6b490
Camilo Pitta <camilo.pitta.c@gmail.com>
2022-09-05 20:36:49 -0500

    vista homev2

--- app.js
+++ app.js
@@ -30,3 +30,7 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/registro.html'));
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
