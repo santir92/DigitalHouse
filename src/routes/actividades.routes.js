71cf9fc6bd355c51d5778de5caf7025ad4d596ee
JerezC <jajerezc.jj@gmail.com>
2022-09-29 21:00:42 -0500

    carga imagenes

--- src/routes/actividades.routes.js
+++ src/routes/actividades.routes.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/actividadesController')
+const multer = require('multer')
 
+const multerDiskStorage = multer.diskStorage({
+    destination: function(req, file, cb) { // request, archivo y callback que almacena archivo en destino
+     cb(null, path.join(__dirname,'../../public/images/actividades')); // Ruta donde almacenamos el archivo
+    },
+    filename: function(req, file, cb) { // request, archivo y callback que almacena archivo en destino
+     let imageName = Date.now() + path.extname(file.originalname);   // milisegundos y extensión de archivo original
+     cb(null, imageName);         
+    }
+});
 
+const uploadFile = multer ({storage: multerDiskStorage})
 
 
 //crear una nueva actividad
 router.get('/create', controller.create)
-//router.post('/create', controller.store)
+//router.post('/create', uploadFile.single('cargarImagen'),controller.store)
 
 //actualizar actividad
 router.get('/update', controller.update);
