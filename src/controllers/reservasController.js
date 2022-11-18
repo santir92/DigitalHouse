692866cf6ab44c504fcf182a1737a4f6523a9b28
santir92 <santir_2004@hotmail.com>
2022-11-18 01:39:21 -0300

    reservas

--- src/controllers/reservasController.js
+++ src/controllers/reservasController.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
 const path = require('path');
+const ejs = require('ejs');
 
+const actividadesFilePath = path.join(__dirname, '../database/actividades.json');
+const actividades = JSON.parse(fs.readFileSync(actividadesFilePath, 'utf-8'));
 
 
 const controller = {
 
 reserva: (req, res) => {
-    res.render('reserva');
+    const actividades = JSON.parse(fs.readFileSync(actividadesFilePath, 'utf-8'));
+    //vista con todas las actividades
+    res.render('reserva', {actividades})
 },
     
 update: (req, res) => {
