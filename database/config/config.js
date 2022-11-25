71297372bf4f14c5774f0d95ac6182b775e23a3f
santir92 <santir_2004@hotmail.com>
2022-11-25 19:22:30 -0300

    con emi

--- database/config/config.js
+++ database/config/config.js
@@ -1,25 +1,27 @@
+
+
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql",
-    "port": process.env.DB_PORT,
-    "operatorsAliases": 0
-    // "username": "g5dh",
-    // "password": "DHg51122",
-    // "database": "g5dh_hosteria",
-    // "host": "mysql-g5dh.alwaysdata.net",
+    // "username": process.env.DB_USER,
+    // "password": process.env.DB_PASS,
+    // "database": process.env.DB_NAME,
+    // "host": process.env.DB_HOST,
     // "dialect": "mysql",
-    // "port": 3306
+    // "port": process.env.DB_PORT,
+    // "operatorsAliases": 0
+    "username": "g5dh",
+    "password": "DHg51122",
+    "database": "g5dh_hosteria",
+    "host": "mysql-g5dh.alwaysdata.net",
+    "dialect": "mysql",
+    "port": 3306
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": "g5dh",
+    "password": "DHg51122",
+    "database": "g5dh_hosteria",
+    "host": "mysql-g5dh.alwaysdata.net",
+    "dialect": "mysql",
   },
   "production": {
     // "username": process.env.DB_USER,
@@ -28,10 +30,10 @@ module.exports = {
     // "host": process.env.DB_HOST,
     // "dialect": "mysql",
     // "operatorsAliases": 0
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": "g5dh",
+    "password": "DHg51122",
+    "database": "g5dh_hosteria",
+    "host": "mysql-g5dh.alwaysdata.net",
+    "dialect": "mysql",
   }
 }
