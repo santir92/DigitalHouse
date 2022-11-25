e932590e36363531a5eec2220732aac3c90a07fa
JerezC <jajerezc.jj@gmail.com>
2022-11-25 09:04:34 -0500

    usuarios

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -1,20 +1,33 @@
-const User = require("../models/User")
+const db = require('../../database/models');
 
 function userLoggedMiddleware(req, res, next){
     res.locals.isLogged = false;
 
     let userInCookie = req.cookies.userName;
-    //PROBAR CON UN IF LA VARIABLE ESTA HACER LA PROMESA SI NO ESPERAR
-    let userFromCookie = User.findUserByField("username", userInCookie);
 
+    let userFromCookie = null;
+    if(userInCookie){
+        
+        db.Persona.findOne({
+            where: {
+                username: userInCookie
+            }
+        }).then(result =>{
+            
+            return userFromCookie = result
+            
+        })
+    }
     if(userFromCookie) {
         req.session.userLogged = userFromCookie;
     }
-
+    
+    
     if(req.session && req.session.userLogged){
-        res.locals.isLogged = true;
         res.locals.userLogged = req.session.userLogged;
+        res.locals.isLogged = true;
     }
+    
     next();
 }
 
