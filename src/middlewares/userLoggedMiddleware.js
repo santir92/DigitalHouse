a9cd3283be28af126c411ba9213d7ae2132d3704
JerezC <jajerezc.jj@gmail.com>
2022-11-24 23:24:27 -0500

    script

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -4,6 +4,7 @@ function userLoggedMiddleware(req, res, next){
     res.locals.isLogged = false;
 
     let userInCookie = req.cookies.userName;
+    //PROBAR CON UN IF LA VARIABLE ESTA HACER LA PROMESA SI NO ESPERAR
     let userFromCookie = User.findUserByField("username", userInCookie);
 
     if(userFromCookie) {
