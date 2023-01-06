2866205bea9728bd048ea7aaaf18f41e44f0a90a
matiromero9 <110431686+matiromero9@users.noreply.github.com>
2023-01-05 21:51:11 -0300

    Delete Documents/Calculadora directory

--- Documents/Calculadora/Calculadora/dividir.js
+++
@@ -1,8 +0,0 @@
-function dividir(numeroA, numeroB){
-        if((numeroA) && (numeroB) == 0){
-            return "No se puede dividir por cero"
-        } else {
-            numeroA / numeroB
-        }
-}
-module.exports = dividir
